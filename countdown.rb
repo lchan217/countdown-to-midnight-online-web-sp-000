570b361876c1db4660c99856d11f2846220d056d
Lizzie Chan <lchan217@gmail.com>
2019-01-28 23:53:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
 def countdown(integer)
-  while integer > 0 
-    puts "#{integer} SECOND(S)!"
-    integer -= 1
-  end
+    while integer > 0 
+      puts "#{integer} SECOND(S)!"
+      integer -=1
+    end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(integer)
-  while integer > 0 
-    puts "#{integer} SECOND(S)!"
-    integer -= 1
-    sleep 1
-  end
-  
+   while integer > 0 
+      puts "#{integer} SECOND(S)!"
+      sleep 1
+      integer -=1
+    end
+  return "HAPPY NEW YEAR!"
 end
